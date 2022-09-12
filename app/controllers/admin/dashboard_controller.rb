701e6bcee654979eb7b49ac8fbddc510bf14105c
Jaedentw <jaeden.t.west@gmail.com>
2022-09-12 16:13:08 -0600

    implements basic admin dashboard, no styling

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,4 +1,14 @@
 class Admin::DashboardController < ApplicationController
   def show
   end
+
+  def products_count
+    @products_count = Product.count()
+  end
+  helper_method :products_count
+
+  def catagories_count
+    @catagories_count = Category.count()
+  end
+  helper_method :catagories_count
 end
